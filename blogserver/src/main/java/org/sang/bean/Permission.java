84fa689ad357175c5e87ba4a5fe6b7bc14259208
caiping <caiping@changingedu.com>
2019-11-06 20:26:20 +0800

    修改登录接口适配自己数据库

---
+++ blogserver/src/main/java/org/sang/bean/Permission.java
@@ -0,0 +1,39 @@
+package com.ctillnow.entry;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Permission implements Serializable {
+    private int pid;
+    private String pname;
+    private String url;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ blogserver/src/main/java/test/TestDigest.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by caiping on 2019/11/6.
+ */
+public class TestDigest {
+}
